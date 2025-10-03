37cb42a81f012575eac7528265f2d2b3a299de99
CosmicFabri <fabrizzio86@outlook.es>
2025-10-03 11:32:31 -0600

    Init: Basic project structure, installed TailwindCSS, PrimeVue and PrimeIcons.

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <div class="flex flex-row gap-x-4 items-center justify-center m-4 w-full">
+      <i class="pi pi-globe" style="font-size: 2rem"></i>
+      <h1 class="text-3xl">Welcome to the Exoplanet Detector!</h1>
+    </div>
+  </header>
+</template>
