1bc7682288a78c7f6c205aea4d2736631153605a
CosmicFabri <fabrizzio86@outlook.es>
2025-10-03 22:46:48 -0600

    feat: Basic layout structure created.

---
+++ src/views/ContentView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex flex-row gap-x-4 p-4 items-center justify-center">
+	<i class="pi pi-globe" style="font-size: 2rem"></i>
+	<h1 class="text-3xl">Welcome to the Exoplanet Detector!</h1>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
