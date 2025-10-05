b9945f3a2f45a4e2f41f24ff85f87480a08ced5c
CosmicFabri <fabrizzio86@outlook.es>
2025-10-05 11:27:08 -0600

    fix: Added proxy for backend server.

--- src/App.vue
+++ src/App.vue
@@ -8,4 +8,6 @@
 import AppFooter from './components/layout/AppFooter.vue'
 import AppHeader from './components/layout/AppHeader.vue'
 import ContentView from './views/ContentView.vue'
+
+console.log("API URL:", import.meta.env.VITE_API_URL)
 </script>
