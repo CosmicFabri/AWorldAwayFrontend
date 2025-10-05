6a734970894111ab06a9fd6520478ba352226e19
CosmicFabri <fabrizzio86@outlook.es>
2025-10-04 21:23:17 -0600

    fix: Deleted unnecessary lines.

--- src/App.vue
+++ src/App.vue
@@ -5,20 +5,7 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue'
 import AppFooter from './components/layout/AppFooter.vue'
 import AppHeader from './components/layout/AppHeader.vue'
 import ContentView from './views/ContentView.vue'
-import { exoplanetsApi } from './api/axios'
-
-onMounted(async () => {
-	try {
-		const response = await exoplanetsApi.get('init')
-		console.log(response);
-		
-	} catch (error) {
-		console.log("There was an error...");
-		
-	}
-})
 </script>
