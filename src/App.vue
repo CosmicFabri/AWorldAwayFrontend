0e2c8bb1e701e6ceb0a43b921ea02ca0217ca985
CosmicFabri <fabrizzio86@outlook.es>
2025-10-04 20:53:55 -0600

    feat: Axios instance created.

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,20 @@
 </template>
 
 <script setup lang="ts">
+import { onMounted } from 'vue'
 import AppFooter from './components/layout/AppFooter.vue'
 import AppHeader from './components/layout/AppHeader.vue'
 import ContentView from './views/ContentView.vue'
+import { exoplanetsApi } from './api/axios'
+
+onMounted(async () => {
+	try {
+		const response = await exoplanetsApi.get('init')
+		console.log(response);
+		
+	} catch (error) {
+		console.log("There was an error...");
+		
+	}
+})
 </script>
