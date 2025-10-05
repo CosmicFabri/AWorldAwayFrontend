95d16b1e2682af24b266a81d03dac770350e6be5
CosmicFabri <fabrizzio86@outlook.es>
2025-10-04 19:22:59 -0600

    feat: Added editable data point table in the Predict section.

--- src/components/content/PredictInputData.vue
+++ src/components/content/PredictInputData.vue
@@ -7,12 +7,13 @@
     <h2 class="text-xl font-semibold text-center">Input data</h2>
     <Select
       v-model="selectedMethod"
+      input-id="input-method"
       :options="methods"
       optionLabel="name"
-      placeholder="Select how to input data"
+      :default-value="methods[1]"
       class="w-full"
+			@change="emitMethodChange"
     />
-
     <FileUpload
       v-if="showFileUpload"
       ref="fileupload"
@@ -31,17 +32,24 @@ import { FileUpload, Select } from 'primevue'
 import ExecuteButton from './ExecuteButton.vue'
 import { computed, ref } from 'vue'
 
-const selectedMethod = ref()
-// const selectedMethodName = computed(() => selectedMethod.name)
+const emit = defineEmits<{
+  (e: 'method-changed', value: string): void
+}>()
 
-const showFileUpload = computed(() => {
-  return selectedMethod.value?.name === 'Upload file'
-})
+const selectedMethod = ref<{ name: string; code: string } | null>(null)
 
 const methods = ref([
-  { name: 'Upload file', code: 'File' },
+	{ name: 'Upload file', code: 'File' },
   { name: 'Enter manually', code: 'Manually' },
 ])
+
+const showFileUpload = computed(() => selectedMethod.value?.name === 'Upload file')
+
+const emitMethodChange = () => {
+  if (selectedMethod.value) {
+    emit('method-changed', selectedMethod.value.code)
+  }
+}
 </script>
 
 <style scoped></style>
